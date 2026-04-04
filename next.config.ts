c4a20779722f763bd24be056fbce4775eeb30dfc
yellhtetaung <yehtetaung.dev@gmail.com>
2026-04-04 12:28:12 +0630

    fix: fixing for deploy issue

--- next.config.ts
+++ next.config.ts
@@ -3,21 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   reactCompiler: true,
-  redirects() {
-    return [
-      {
-        source: "/school",
-        destination: "/school/dashboard",
-        permanent: true,
-      },
-    ];
-  },
-  outputFileTracingIncludes: {
-    "/*": [
-      "node_modules/pdfkit/js/data/*",
-      "node_modules/next/dist/compiled/@vercel/og/Geist-Regular.ttf",
-    ],
-  },
 };
 
 export default nextConfig;
